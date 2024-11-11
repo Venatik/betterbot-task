d5a296ea68c350c4bb9238d9e47bdbe734e114bc
Venatik <stefan.trajkovski008@gmail.com>
2024-11-11 12:34:57 +0100

    moved to root

---
+++ .angular/cache/18.2.11/betterbot-app/vite/deps/@angular_animations.js
@@ -0,0 +1,53 @@
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-RA3KOFCT.js";
+import "./chunk-KN2UL7QB.js";
+import "./chunk-AK6PRDCJ.js";
+import "./chunk-KQ2POAQN.js";
+import "./chunk-UYPYIYP4.js";
+import "./chunk-DNYJFIXW.js";
+import "./chunk-EPAV4CNQ.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
