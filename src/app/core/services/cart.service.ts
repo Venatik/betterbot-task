3026ba0506860eb6571c31dfc35af020148c605a
Venatik <stefan.trajkovski008@gmail.com>
2024-11-13 17:55:57 +0100

    final

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -5,8 +5,26 @@ import { Product } from "../../types/product.interface";
   providedIn: "root",
 })
 export class CartService {
-  private cartItems = signal<(Product & { cartId: number })[]>([]);
-  private currentId = 0;
+  private cartItems = signal<(Product & { cartId: number })[]>(
+    this.loadCartFromStorage()
+  );
+  private currentId = this.getLastUsedId();
+
+  private loadCartFromStorage() {
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+  }
+
+  private getLastUsedId() {
+    const savedCart = this.loadCartFromStorage();
+    return savedCart.length > 0
+      ? Math.max(...savedCart.map((item: { cartId: any }) => item.cartId)) + 1
+      : 0;
+  }
+
+  private saveCartToStorage() {
+    localStorage.setItem("cart", JSON.stringify(this.cartItems()));
+  }
 
   addToCart(product: Product) {
     const currentItems = this.cartItems();
@@ -14,6 +32,7 @@ export class CartService {
       ...currentItems,
       { ...product, cartId: this.currentId++ },
     ]);
+    this.saveCartToStorage();
   }
 
   removeFromCart(itemToRemove: Product & { cartId: number }) {
@@ -22,6 +41,7 @@ export class CartService {
       item => item.cartId !== itemToRemove.cartId
     );
     this.cartItems.set(updatedItems);
+    this.saveCartToStorage();
   }
 
   getCartItems() {
